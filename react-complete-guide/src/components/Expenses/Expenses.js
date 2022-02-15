a2d480275b89549e6e06dcd525dce71c96e30be7
Nihinlola Fajemilehin <nihinfaj@gmail.com>
2022-02-15 17:03:04 +0000

    upload files

---
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import React, { useState } from "react";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  function saveSelectedDateHandler(dateValue) {
+    setFilteredYear(dateValue);
+  }
+
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onPickDate={saveSelectedDateHandler}
+      />
+      <ExpensesChart expenses={filteredExpenses} />
+      <ExpensesList items={filteredExpenses} />
+    </Card>
+  );
+}
+
+export default Expenses;
